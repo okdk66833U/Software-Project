b5a0414d93306288d4f0ff89dba0a9e86845fe4a
okdk66833U <okdk66833@kookmin.ac.kr>
2025-09-15 14:49:12 +0900

    Updated 05_practice_2.ino

--- 250915/05_practice_2/05_practice_2.ino
+++ 250915/05_practice_2/05_practice_2.ino
@@ -1,16 +1,14 @@
 #define PIN_LED 7
-unsigned int toggle;
+
 void setup() {
   pinMode(PIN_LED, OUTPUT);
-  toggle=0;
 }
 
 void loop() {
-  digitalWrite(PIN_LED, toggle);
+  digitalWrite(PIN_LED, 0);
   delay(1000);
   for(int i=0;i<11;i++){
-    toggle=!toggle;
-    digitalWrite(PIN_LED, toggle);
+    digitalWrite(PIN_LED, !(i%2));
     delay(100);
   }
   while(1){}
